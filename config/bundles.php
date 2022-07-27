0dd17a75cca5c6426a73916d4586efa7513e0ae8
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2022-07-27 09:29:21 +0200

    Initial project setup. Installed

---
+++ config/bundles.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Anzu\CommonBundle\AnzuCommonBundle::class => ['all' => true],
+    Anzu\AuthenticationBundle\AnzuAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    PetitPress\GpsMessengerBundle\GpsMessengerBundle::class => ['all' => true],
+];
