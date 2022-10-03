db14e095169094cc539f9620007c4d3c7826b49e
Ronald Marfoldi <ronald.marfoldi@petitpress.sk>
2022-10-03 10:09:09 +0200

    Fix BaseVoter for acl

--- src/Security/Voter/BaseVoter.php
+++ src/Security/Voter/BaseVoter.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace App\Security\Voter;
 
-use AnzuSystems\CoreDamBundle\Permission\DamPermissions;
+use App\Security\Permission\DamPermissions;
 
 final class BaseVoter extends AbstractVoter
 {
     protected function getSupportedPermissions(): array
     {
-        return [
-            DamPermissions::DAM_ASSET_VIEW,
-        ];
+        return DamPermissions::all();
     }
 }
