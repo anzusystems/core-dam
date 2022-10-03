31369eba2e636480a59137eaf41c61831e059a89
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2022-10-03 12:59:48 +0200

    GCS flysystem config.

---
+++ config/services/bucket.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Google\Cloud\Storage\StorageClient;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+return static function (ContainerConfigurator $configurator): void {
+    $services = $configurator->services();
+
+    $services
+        ->defaults()
+            ->autowire(true)
+            ->autoconfigure(true);
+
+    $services->set('anzu.google_storage.env_client')
+        ->class(StorageClient::class)
+        ->args([
+            [
+                'projectId' => 'anzu-devel-pp',
+                'keyFilePath' => env('resolve:string:GOOGLE_BUCKET_CREDENTIALS')
+            ]
+        ])
+    ;
+};
