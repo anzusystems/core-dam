0b546cbe7ad9bed6da4e462b034a90c1cc10e894
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2022-10-03 10:01:13 +0200

    Added migrations.

--- src/Migrations/Version20221003075735.php
+++ src/Migrations/Version20221003075735.php
@@ -7,18 +7,13 @@ namespace App\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20220921131711 extends AbstractMigration
+final class Version20221003075735 extends AbstractMigration
 {
-    public function isTransactional(): bool
-    {
-        return false;
-    }
-
     public function up(Schema $schema): void
     {
         $this->addSql("INSERT INTO `user` (id, email, roles, permissions, enabled) VALUES
-            (1, 'dam_anonymous@anzusystems.dev', '[\"ROLE_USER\"]', '[]', 0),                                                       
-            (2, 'dam_console@anzusystems.dev', '[\"ROLE_USER\"]', '[]', 0)                                                       
+            (1, 'dam_anonymous@anzusystems.dev', '[\"ROLE_USER\"]', '[]', 0),
+            (2, 'dam_console@anzusystems.dev', '[\"ROLE_USER\"]', '[]', 0)
         ");
     }
 
