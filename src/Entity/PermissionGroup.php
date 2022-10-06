5b577f92162e559b4b1cbc9df65f106ce3a63b19
Ronald Marfoldi <ronald.marfoldi@petitpress.sk>
2022-10-06 13:50:40 +0200

    Recreated doctrine migrations

--- src/Entity/PermissionGroup.php
+++ src/Entity/PermissionGroup.php
@@ -57,7 +57,7 @@ class PermissionGroup implements IdentifiableInterface, UserTrackingInterface, T
      * List of users who belongs to permission group.
      */
     #[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'permissionGroups', indexBy: 'id')]
-    #[Serialize(type: EntityIdHandler::class)]
+    #[Serialize(handler: EntityIdHandler::class, type: User::class)]
     private Collection $users;
 
     public function __construct()
