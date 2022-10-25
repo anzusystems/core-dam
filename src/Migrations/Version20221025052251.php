d0c4350b11014223e949533a2a52debf92a8aad8
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2022-10-25 07:23:33 +0200

    Added author identifier migration.

---
+++ src/Migrations/Version20221025052251.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20221025052251 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE author ADD identifier VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_BDAFD8C85E237E06772E836AE961F7A ON author (name, identifier, ext_system_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX UNIQ_BDAFD8C85E237E06772E836AE961F7A ON author');
+        $this->addSql('ALTER TABLE author DROP identifier');
+    }
+}
