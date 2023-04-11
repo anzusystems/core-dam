8e4cbf6c4e4917f58d3bb3542b75cd167e6e4624
Tomáš Hermánek <tomas.hermanek@petitpress.sk>
2023-04-11 12:50:33 +0000

    Merged PR 5814: Update

--- tests/Controller/Api/Adm/V1/ArtemisVideoDistributionControllerTest.php
+++ tests/Controller/Api/Adm/V1/ArtemisVideoDistributionControllerTest.php
@@ -26,7 +26,7 @@ final class ArtemisVideoDistributionControllerTest extends AbstractApiController
         'keywords' => ['News', 'Podcast', 'Politics'],
         'authors' => ['Aarne Ormonde', 'Larry Queen', 'Malka Raisa'],
         'createArticle' => false,
-        'rubricId' => 3,
+        'rubricId' => 7026,
     ];
 
     private DistributionManagerProvider $distributionManagerProvider;
