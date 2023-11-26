c0a5144769be3a2915003c05d708facfbb3f80d1
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2023-11-26 20:29:40 +0100

    fix

--- src/Migrations/Version20231019105238.php
+++ src/Migrations/Version20231019105238.php
@@ -16,7 +16,9 @@ final class Version20231019105238 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        // todo uncomment after release
 //        $this->addSql('ALTER TABLE custom_form_element DROP key_name');
+        $this->addSql('ALTER TABLE custom_form_element ALTER  key_name SET DEFAULT \'\'');
         $this->addSql('ALTER TABLE asset_file ADD asset_attributes_convert_to_mime VARCHAR(32) NOT NULL');
     }
 
