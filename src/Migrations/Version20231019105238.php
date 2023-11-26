6ddd5e6d99792ac34048f2bd677360ff6eb30f49
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2023-11-26 18:33:12 +0100

    tmp comment drop column.

--- src/Migrations/Version20231019105238.php
+++ src/Migrations/Version20231019105238.php
@@ -16,7 +16,7 @@ final class Version20231019105238 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE custom_form_element DROP key_name');
+//        $this->addSql('ALTER TABLE custom_form_element DROP key_name');
         $this->addSql('ALTER TABLE asset_file ADD asset_attributes_convert_to_mime VARCHAR(32) NOT NULL');
     }
 
