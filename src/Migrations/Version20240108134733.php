084062181a9ebe37f0f22d68de21494769193e03
Tomas Hermanek <tomas.hermanek@petitpress.sk>
2024-01-09 15:22:48 +0100

    fixed DB migration

--- src/Migrations/Version20240108134733.php
+++ src/Migrations/Version20240108134733.php
@@ -14,12 +14,18 @@ final class Version20240108134733 extends AbstractMigration
         $this->addSql('
             UPDATE custom_form_element SET attributes_type = \'integer\' where attributes_type = \'number\'
         ');
+        $this->addSql('
+            UPDATE custom_form_element SET attributes_readonly = 1 where property = \'mediaApiPaths\' OR property = \'mediaApiIds\'
+        ');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('
             UPDATE custom_form_element SET attributes_type = \'number\' where attributes_type = \'integer\'
+        ');
+        $this->addSql('
+            UPDATE custom_form_element SET attributes_readonly = 0 where property = \'mediaApiPaths\' OR property = \'mediaApiIds\'
         ');
     }
 }
