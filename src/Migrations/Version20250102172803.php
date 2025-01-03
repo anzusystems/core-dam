849b98fa56341b6a3e678651bd6e6d857795ebb9
Tomáš Hermánek <tomas.hermanek@petitpress.sk>
2025-01-03 09:43:58 +0000

    Merged PR 11252: Added IDX_licence_attributes_status_url to asset_file
    Added IDX_licence_attributes_status_url to asset_file

---
+++ src/Migrations/Version20250102172803.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250102172803 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE INDEX IDX_licence_attributes_status_url ON asset_file (licence_id, asset_attributes_status, asset_attributes_origin_url(255))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX IDX_licence_attributes_status_url ON asset_file');
+    }
+}
