f38b7c52c5d05c81a423ad02e03a173f57053f9e
Martin Práznovský <martin.praznovsky@petitpress.sk>
2026-02-02 06:47:49 +0100

    refs #84212 [ApiParams] Extend ApiPubParams with limit value 1

--- src/Model/Request/ApiPubParams.php
+++ src/Model/Request/ApiPubParams.php
@@ -17,7 +17,7 @@ final readonly class ApiPubParams
     /**
      * @var list<int>
      */
-    public const array ALLOWED_LIMITS = [10, 20, 50];
+    public const array ALLOWED_LIMITS = [1, 10, 20, 50];
 
     private const string PAGE = 'page';
     private const string EXCLUDE_IDS = 'excludeIds';
